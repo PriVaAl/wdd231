a6ca341ec623ea74aae0ae25148d665ebc4ddb0a
PriVaAl <priscilla.valverde@gmail.com>
2025-03-13 21:36:52 -0600

    color view

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -16,6 +16,25 @@ const lastModifiedElement = document.querySelector("#lastModified")
 lastModifiedElement.textContent = "Last Modification: " + document.lastModified;
 });
 
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener('click', () => {
+	// example using arrow function
+	display.classList.add('grid');
+	display.classList.remove('list');
+});
+
+listbutton.addEventListener('click', showList); // example using defined function
+
+function showList() {
+	display.classList.add('list');
+	display.classList.remove('grid');
+}
+
 const url = "data/members.json"; 
 const businessContainer = document.getElementById("business");
 console.log("Directory container:", businessContainer); // The container for business cards
